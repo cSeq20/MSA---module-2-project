1be09af74e5abc6ec21800da4d6dbac4cf985b69
Chris Sequeira <Chris Sequeira>
2016-09-15 21:54:02 +1200

    small change

--- js/script.ts
+++ js/script.ts
@@ -1,4 +1,5 @@
 var searchBtn = $('#btnGetWeather')[0];
+var btnValue = $('#moreDetails').attr('value');         // get value of forecast button.
 
 // queries api when search button is pressed and retrieves data to be displayed on webpage.
 searchBtn.addEventListener("click", function () {
@@ -50,7 +51,7 @@ moreInfo.addEventListener("click", function () {
 
 // function that changes value of button to show less / get 5 day forecast.
 function changeButton(): void {
-    var btnValue = $('#moreDetails').attr('value');         // get value of button.
+    
     // Changes value of button according to its current value.
     if (btnValue == 'Get 5 Day Forecast') {
         $('#moreDetails').prop('value', 'Show less');
